4fd068ad918b60d69ba838238bfb95f3d496fd5a
Jonathan Zarate <zaratedev@gmail.com>
2024-07-31 14:15:04 -0600

    Layout for example

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,9 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div>
+                    <img class="flex-shrink-0 w-10 h-10 bg-gray-300 rounded-full" src="{{ asset('images/cody.jpg')}}" alt="Avatar de usuario">
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
